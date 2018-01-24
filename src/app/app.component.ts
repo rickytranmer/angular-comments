c56653fe18a8dddeaed155a0c2db2b78df8410d7
rickytranmer <rwtranmer@gmail.com>
2018-01-24 16:27:21 -0700

    edit comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,9 @@ export class AppComponent {
       {author: 'Sue', comment: 'I would also like to congratulate you!'}
   ];
 
+  author :string;
+  comment :string;
+
   submitComment(thisAuthor, thisComment) {
   	this.comments.push({author: thisAuthor, comment: thisComment});
   	console.log(this.comments);
@@ -21,5 +24,12 @@ export class AppComponent {
   	console.log(id);
   	this.comments.splice(id,1);
   	console.log(this.comments);
+  }
+
+  editComment(id) {
+  	this.author = this.comments[id].author;
+  	this.comment = this.comments[id].comment;
+  	this.deleteComment(id);
+  	console.log(id);
   }
 }
